d059020346cce633abe4de1f4a27f92cbd3088e9
Trikita73 <diachenkoandrii7@gmail.com>
2024-05-24 23:13:36 +0300

    index_js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (_, res) => res.send('<h1>OK</h1>'))
+app.get('/', (_, res) => res.send('<h1>OK!</h1>'))
 
 const port = 8080
 
